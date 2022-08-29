9fc9300bc719b570598a5f096d43150a872d81c7
Ahmed <ahmedheaba201@gmail.com>
2022-08-30 01:12:10 +0200

    Initialized

---
+++ udagram/set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=myPassword
+export POSTGRES_HOST=database-2.cagqkofoomfs.us-east-1.rds.amazonaws.com
+export POSTGRES_PORT=5432
+export POSTGRES_DB=postgres
+export AWS_BUCKET=s3://elasticbeanstalk-us-east-1-685321540331/
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://my-685321540331-bucket.s3-website-us-east-1.amazonaws.com/
